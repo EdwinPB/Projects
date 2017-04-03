4342acbcd2a316dd7fe6ca6e86d706e8445b095c
Edwin <epolo@wawand.co>
2017-04-03 11:14:10 -0500

    Initial Commit

---
+++ InternationalizationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternationalizationApp
+//
+//  Created by Edwin Polo on 4/3/17.
+//  Copyright © 2017 Edwin Polo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
